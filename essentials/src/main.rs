3c69b9fdcdf3c05943969f36cd0f0679b54f3373
Diaa ElKott <elkott@gmail.com>
2023-03-09 21:49:05 -0500

    Reorganize code files.

--- essentials/src/main.rs
+++ essentials/src/main.rs
@@ -1,22 +1,27 @@
 mod enums;
 mod structs;
 
-include!("hello_rust.rs");
-include!("variables.rs");
+// include!("00_hello_rust.rs");
+// include!("01_variables.rs");
+include!("02_ownership.rs");
 // include!("structs.rs");
 // include!("enums.rs");
 // include!("struct_enum.rs");
 // include!("lifetimes.rs");
 // include!("date_example.rs");
 
-mod date_example;
+// mod date_example;
+
 fn main() {
     // // HelloRust - a guessing game.
     // guess_game();
 
     // // Variables, loops, and functions.
 
-    run_variablaes_demo();
+    // run_variablaes_demo();
+
+    // Ownership.
+    run_ownership_demo();
 
     // structs_demo();
 
